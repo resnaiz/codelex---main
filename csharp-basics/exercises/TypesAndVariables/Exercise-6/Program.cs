933e2289a9668cc53adc47459ac83040f4aeb201
Arnis Pētersons <arnisp03@gmail.com>
2023-02-16 20:44:28 +0200

    Exercise-6 done

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise-6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, sum = 0, m;
+
+            Console.WriteLine("Enter a number:");
+
+            n = int.Parse(Console.ReadLine());
+
+            while (n > 0)
+            {
+                m = n % 10;
+                sum += m;
+                n /= 10;
+            }
+
+            Console.WriteLine("Sum is = " + sum);
+        }
+    }
+}
