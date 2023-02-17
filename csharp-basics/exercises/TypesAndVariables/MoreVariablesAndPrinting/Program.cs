047d182f87964a17824d31fd7f2a1ebeeeef0551
Arnis Pētersons <arnisp03@gmail.com>
2023-02-17 21:00:52 +0200

    MoreVariablesAndPrinting changed.

--- csharp-basics/exercises/TypesAndVariables/MoreVariablesAndPrinting/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/MoreVariablesAndPrinting/Program.cs
@@ -8,26 +8,26 @@ namespace MoreVariablesAndPrinting
     {
         static void Main(string[] args)
         {
-            string Name, Eyes, Teeth, Hair;
-            int Age;
-            double Height, Weight;
+            string personName, eyesColor, teethColor, hairColor;
+            int personAge;
+            double personHeight, personWeight;
 
-            Name = "Zed A. Shaw";
-            Age = 35;
-            Height = 74 * 2.54;
-            Weight = 180 * 0.453592;
-            Eyes = "Blue";
-            Teeth = "White";
-            Hair = "Brown";
+            personName = "Zed A. Shaw";
+            personAge = 35;
+            personHeight = 74 * 2.54;
+            personWeight = 180 * 0.453592;
+            eyesColor = "Blue";
+            teethColor = "White";
+            hairColor = "Brown";
 
-            Console.WriteLine($"Let's talk about {Name}.");
-            Console.WriteLine($"He's {Height:F2} centimeters tall.");
-            Console.WriteLine($"He's {Weight:F2} kilograms heavy.");
+            Console.WriteLine($"Let's talk about {personName}.");
+            Console.WriteLine($"He's {personHeight:F2} centimeters tall.");
+            Console.WriteLine($"He's {personWeight:F2} kilograms heavy.");
             Console.WriteLine("Actually, that's not too heavy.");
-            Console.WriteLine($"He's got {Eyes} eyes and {Hair} hair.");
-            Console.WriteLine($"His teeth are usually {Teeth} depending on the coffee.");
+            Console.WriteLine($"He's got {eyesColor} eyes and {hairColor} hair.");
+            Console.WriteLine($"His teeth are usually {teethColor} depending on the coffee.");
 
-            Console.WriteLine($"If I add {Age}, {Height:F2} centimeters, and {Weight:F2} kilograms, I get {(Age + Height + Weight):F2}.");
+            Console.WriteLine($"If I add {personAge}, {personHeight:F2} centimeters, and {personWeight:F2} kilograms, I get {(personAge + personHeight + personWeight):F2}.");
 
             Console.ReadKey();
         }
