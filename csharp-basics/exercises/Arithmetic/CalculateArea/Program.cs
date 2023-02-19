159a32614f1fe7166bf48e2e1bc64c1dccbfd7dd
Arnis Pētersons <arnisp03@gmail.com>
2023-02-19 17:28:39 +0200

    Exercise #11 done.

--- csharp-basics/exercises/Arithmetic/CalculateArea/Program.cs
+++ csharp-basics/exercises/Arithmetic/CalculateArea/Program.cs
@@ -10,46 +10,61 @@ namespace CalculateArea
     {
         static void Main(string[] args)
         {
-            // Get the user's menu choice.
             int choice = GetMenu();
 
-            while (choice != 0)
+            while (choice != 4)
             {
-
+                switch (choice)
+                {
+                    case 1:
+                        CalculateCircleArea();
+                        break;
+                    case 2:
+                        CalculateRectangleArea();
+                        break;
+                    case 3:
+                        CalculateTriangleArea();
+                        break;
+                }
+                choice = GetMenu();
             }
+
+            Console.WriteLine("Exiting program...");
         }
 
         public static int GetMenu()
         {
-
-            int userChoice;
-            
-
-            // Display the menu.
             Console.WriteLine("Geometry Calculator\n");
             Console.WriteLine("1. Calculate the Area of a Circle");
             Console.WriteLine("2. Calculate the Area of a Rectangle");
             Console.WriteLine("3. Calculate the Area of a Triangle");
             Console.WriteLine("4. Quit\n");
             Console.WriteLine("Enter your choice (1-4) : ");
+            int userChoice = int.Parse(Console.ReadLine());
             var keyboard = Console.ReadKey();
-            // get input from user
-
-            // validate input
+            while (userChoice < 1 || userChoice > 4)
+            {
+                Console.WriteLine(" - Invalid choice. Please enter a number from 1 to 4.");
+                Console.Write("Enter your choice (1-4): ");
+                userChoice = int.Parse(Console.ReadLine());
+            }
 
             return userChoice;
         }
 
         public static void CalculateCircleArea()
         {
-            // Get input from user
+            
             Console.WriteLine("What is the circle's radius? ");
             //todo
+            string input = Console.ReadLine();
+            double radius = Convert.ToDouble(input);
+            decimal decimalRad = Convert.ToDecimal(radius);
 
 
             // Display output
             Console.WriteLine("The circle's area is "
-                    + Geometry.AreaOfCircle(radius));
+                    + Geometry.AreaOfCircle(decimalRad));
         }
 
         public static void CalculateRectangleArea()
@@ -61,10 +76,12 @@ namespace CalculateArea
 
             // Get length
             Console.WriteLine("Enter length? ");
+            length = Convert.ToDecimal(Console.ReadLine());
             //todo
 
             // Get width
             Console.WriteLine("Enter width? ");
+            width = Convert.ToDecimal(Console.ReadLine());
             //todo
 
             // Display output
@@ -83,12 +100,12 @@ namespace CalculateArea
             Console.WriteLine("Enter length of the triangle's base? ");
             //todo
             //read key value
-
+            ground = Convert.ToDecimal(Console.ReadLine());
             // Get the height
             Console.WriteLine("Enter triangle's height? ");
             //todo
             //read key value
-
+            height= Convert.ToDecimal(Console.ReadLine());
             // Display the triangle's area.
             Console.WriteLine("The triangle's area is "
                     + Geometry.AreaOfRectangle(ground, height));
---
+++ csharp-basics/exercises/Arithmetic/Exercise-11/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+public class Program
+{
+    public static string Moran(int num)
+    {
+        // Find the sum of digits
+        int rem = 0;
+        int sum = 0;
+        int n = num;
+
+        while (num > 0)
+        {
+            rem = num % 10;
+            sum = sum + rem;
+            num = num / 10;
+        }
+
+        // Check if the number is a Moran number
+        bool isMoran = false;
+        if (n % sum == 0)
+        {
+            int quotient = n / sum;
+            for (int i = 2; i <= Math.Sqrt(quotient); i++)
+            {
+                if (quotient % i == 0)
+                {
+                    isMoran = false;
+                    break;
+                }
+                else
+                {
+                    isMoran = true;
+                }
+            }
+        }
+
+        // Output the result
+        if (isMoran)
+        {
+            return "M";
+        }
+        else if (n % sum == 0)
+        {
+            return "H";
+        }
+        else
+        {
+            return "Neither";
+        }
+    }
+    public static void Main()
+    {
+        Console.WriteLine(Moran(132)); // Output: H
+        Console.WriteLine(Moran(133)); // Output: M
+        Console.WriteLine(Moran(134));
+        Console.WriteLine(Moran(135));// Output: Neither
+    }
+}
