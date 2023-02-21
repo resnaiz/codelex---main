3f97bb066a2331141d780e78d1529213e3fab4df
Arnis Pētersons <arnisp03@gmail.com>
2023-02-21 19:28:53 +0200

    Exercise 8 done.

---
+++ csharp-basics/exercises/Arithmetic/Exercise_8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
