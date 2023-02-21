f13ef2002011fab6fc94f4aa01b4b1b5f5146201
Arnis Pētersons <arnisp03@gmail.com>
2023-02-21 08:19:42 +0200

    GetTheCentury done.

--- csharp-basics/exercises/FlowOfControl/FlowControl/GetTheCentury/Program.cs
+++ csharp-basics/exercises/FlowOfControl/FlowControl/GetTheCentury/Program.cs
@@ -6,7 +6,29 @@ namespace GetTheCentury
     {
         static void Main(string[] args)
         {
+            int firstYear = 1756;
+            int secondYear = 1555;
+            int thirdYear = 1000;
+            int fourthYear = 1001;
+            int fifthYear = 2003;
 
+            Console.WriteLine($"Century of the year: {firstYear} - " + centuryGet(firstYear));
+            Console.WriteLine($"Century of the year: {secondYear} - " + centuryGet(secondYear));
+            Console.WriteLine($"Century of the year: {thirdYear} - " + centuryGet(thirdYear));
+            Console.WriteLine($"Century of the year: {fourthYear} - " + centuryGet(fourthYear));
+            Console.WriteLine($"Century of the year: {fifthYear} - " + centuryGet(fifthYear));
+        }
+        public static string centuryGet(int year)
+        {
+            int num = year % 100 == 0 ? year / 100 : year / 100 + 1;
+            if(num > 20)
+            {
+                return num + "st century";
+            }
+            else
+            {
+                return num + "th century";
+            }
         }
     }
 }
