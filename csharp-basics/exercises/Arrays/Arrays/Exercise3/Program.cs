754fa0fb13de2bab387aab35570f258d81a9be9c
Arnis Pētersons <arnisp03@gmail.com>
2023-02-24 15:13:21 +0200

    changes

--- csharp-basics/exercises/Arrays/Arrays/Exercise3/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise3/Program.cs
@@ -5,7 +5,6 @@ namespace Exercise3
 {
     class Program
     {
-        //TODO: Write a C# program to calculate the average value of array elements.
         private static void Main(string[] args)
         {
             int[] numbers = {20, 30, 25, 35, -16, 60, -100};
