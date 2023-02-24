0a93f5665f6fa16096d0f0350f05fd06956e13bc
Arnis Pētersons <arnisp03@gmail.com>
2023-02-24 19:21:53 +0200

    Exercise 7 done.

---
+++ csharp-basics/exercises/Loops/Loops/Exercise 7/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Exercise_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Piglet!");
+
+            int score = 0;
+            bool gameIsOver = false;
+
+            while (!gameIsOver)
+            {
+                int newRoll = RollOver();
+
+                if (newRoll == 1)
+                {
+                    Console.WriteLine("You rolled a 1!");
+                    Console.WriteLine("You got 0 points.");
+                    gameIsOver = true;
+                }
+                else
+                {
+                    Console.WriteLine($"You rolled a {newRoll}");
+                    score += newRoll;
+                    Console.WriteLine("Roll again?");
+                    string newInput = Console.ReadLine();
+
+                    if (newInput == "y" || newInput == "yes")
+                    {
+                        continue;
+                    }     
+                    else if (newInput == "n" || newInput == "no")
+                    {
+                        Console.WriteLine($"You got {score} points.");
+                        gameIsOver = true;
+                    }
+                }
+            }
+        }
+
+        static int RollOver()
+        {
+            Random randomNum = new Random();
+            return randomNum.Next(1, 7);
+        }
+    }
+}
