39192bd9990a0d345e70243754caf38f06eaa23b
Arnis Pētersons <arnisp03@gmail.com>
2023-02-26 14:55:58 +0200

    Exercise 8 changed

--- csharp-basics/exercises/Arrays/Arrays/Exercise 8/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise 8/Program.cs
@@ -8,68 +8,87 @@ namespace Exercise_8
     {
         static void Main(string[] args)
         {
-            string[] listOfWords = { "leviathan", "monster", "mammoth", "behemoth", "colossus" };
-            string oneWord = listOfWords[new Random().Next(listOfWords.Length)];
-            string newGuess = "";
-            string newMiss = "";
-            int tries = 6;
+            bool playGame = true;
 
-            Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-            Console.WriteLine();
-            
-            while (true)
+            while (playGame)
             {
-                Console.Write("Word:\t");
-                foreach (char c in oneWord)
+                string[] listOfWords = { "leviathan", "monster", "mammoth", "behemoth", "colossus" };
+                string oneWord = listOfWords[new Random().Next(listOfWords.Length)];
+                string newGuess = "";
+                string newMiss = "";
+                int tries = 6;
+
+                Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+                Console.WriteLine();
+
+                while (true)
                 {
-                    if (newGuess.Contains(c))
+                    Console.Write("Word:\t");
+                    foreach (char c in oneWord)
                     {
-                        Console.Write(c);
+                        if (newGuess.Contains(c))
+                        {
+                            Console.Write(c);
+                        }
+                        else
+                        {
+                            Console.Write("_ ");
+                        }
+                    }
+
+                    Console.WriteLine("\nMisses:\t{0}", newMiss);
+
+                    Console.Write("Guess:\t");
+                    char guess = char.ToLower(Console.ReadKey().KeyChar);
+                    Console.WriteLine();
+
+                    if (oneWord.Contains(guess))
+                    {
+                        newGuess += guess;
                     }
                     else
                     {
-                        Console.Write("_ ");
+                        newMiss += guess;
+                        tries--;
                     }
-                }
 
-                Console.WriteLine("\nMisses:\t{0}", newMiss);
+                    if (tries == 0)
+                    {
+                        Console.WriteLine("You have run out of attempts. Word: '{0}'.", oneWord);
+                        break;
+                    }
 
-                Console.Write("Guess:\t");
-                char guess = char.ToLower(Console.ReadKey().KeyChar);
-                Console.WriteLine();
+                    if (oneWord.All(c => newGuess.Contains(c)))
+                    {
+                        Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+                        Console.WriteLine();
+                        Console.WriteLine("Word:\t{0}", oneWord.ToLower());
+                        Console.WriteLine("Misses:\t{0}", newMiss);
+                        Console.WriteLine("YOU GOT IT!");
+                        break;
+                    }
 
-                if (oneWord.Contains(guess))
-                {
-                    newGuess += guess;
-                }
-                else
-                {
-                    newMiss += guess;
-                    tries--;
-                }
-
-                if(tries == 0)
-                {
-                    Console.WriteLine("You have run out of attempts. Word: '{0}'.", oneWord);
-                    break;
-                }
-
-                if (oneWord.All(c => newGuess.Contains(c)))
-                {
-                    Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-");
                     Console.WriteLine();
-                    Console.WriteLine("Word:\t{0}", oneWord.ToLower());
-                    Console.WriteLine("Misses:\t{0}", newMiss);
-                    Console.WriteLine("YOU GOT IT!");
-                    break;
                 }
 
-                Console.WriteLine();
-                Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-                Console.WriteLine();
-            }
+                Console.WriteLine("\nPlay 'again' or 'quit'?");
+                string input = Console.ReadLine();
 
-            Console.WriteLine("\nPlay 'again' or 'quit'? quit");
+                while (input != "again" && input != "quit")
+                {
+                    Console.WriteLine("Invalid input.");
+                    input = Console.ReadLine();
+                }
+
+                if (input == "again")
+                {
+                    continue;
+                }
+                else if (input == "quit")
+                {
+                    return;
+                }
+            }
         }
     }
 }
