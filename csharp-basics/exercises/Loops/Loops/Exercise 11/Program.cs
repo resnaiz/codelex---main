530a5013ed43551ad5bc7f786d10bee273f59d6c
Arnis Pētersons <arnisp03@gmail.com>
2023-02-27 12:35:41 +0200

    Exercise 11 done.

---
+++ csharp-basics/exercises/Loops/Loops/Exercise 11/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercise_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the sentence: ");
+            string input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string result = "";
+                char c = input[i];
+
+                if(Char.IsUpper(c))
+                {
+                    result += Char.ToLower(c);
+                }
+                else if (Char.IsLower(c))
+                {
+                    result += Char.ToUpper(c);
+                }
+                else
+                {
+                    result += c;
+                }
+
+                Console.Write(result);
+            }
+        }
+    }
+}
