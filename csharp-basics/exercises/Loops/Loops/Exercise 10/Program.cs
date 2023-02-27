7ed00c2b81f8f1c7cf70873c9640572f9d93c1b9
Arnis Pētersons <arnisp03@gmail.com>
2023-02-27 12:28:07 +0200

    Exercise 10 done.

---
+++ csharp-basics/exercises/Loops/Loops/Exercise 10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercise_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Min? ");
+            int input = int.Parse(Console.ReadLine());
+            Console.Write("Max? ");
+            int otherInput = int.Parse(Console.ReadLine());
+
+            int squareSize = otherInput - input + 1;
+
+            for(int row = 0; row < squareSize; row++)
+            {
+                for(int col = 0; col < squareSize; col++)
+                {
+                    int value = input + ((row + col) % squareSize);
+                    Console.Write(value);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
