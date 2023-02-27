84b43a810e785ad4b06d5303997e3bff1382285d
Arnis Pētersons <arnisp03@gmail.com>
2023-02-27 11:48:51 +0200

    Exercise 8 done.

---
+++ csharp-basics/exercises/Loops/Loops/Exercise8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercise8
+{
+    internal class Program
+    {
+        const int FigureSize = 3;
+
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < FigureSize; i++)
+            {
+                for(int r = 0; r < (FigureSize - i - 1) * 4; r++)
+                {
+                    Console.Write("/");
+                }
+
+                for(int r = 0; r < i * 8; r++)
+                {
+                    Console.Write("*");
+                }
+
+                for(int r = 0; r < (FigureSize - i - 1) * 4; r++)
+                {
+                    Console.Write("\\");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
