0d3a71fa9dc2630fc35f1f3b24b6f70587c0248b
Arnis Pētersons <arnisp03@gmail.com>
2023-02-28 16:06:20 +0200

    Exercise 6 done.

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 6/Dog.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_6
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public string Sex { get; set; }
+        public string Mother { get; set; }
+        public string Father { get; set; }
+
+        public Dog(string name, string sex)
+        {
+            Name = name;
+            Sex = sex;
+        }
+
+        public void SetMother(string mother)
+        {
+            Mother = mother;
+        }
+        
+        public void SetFather(string father)
+        {
+            Father = father;
+        }
+
+        public bool HasSameMotherAs(Dog otherDog)
+        {
+            if (otherDog.Mother == Mother)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public string FathersName()
+        {
+            if(Father == null)
+            {
+                return "Unknown";
+            }
+
+            return Father;
+        }
+    }
+}
