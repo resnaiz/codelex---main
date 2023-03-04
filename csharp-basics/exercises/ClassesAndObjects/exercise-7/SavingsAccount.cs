c2cc17029b3c4d3f6ac961611fd08d1f4027b576
Arnis Pētersons <arnisp03@gmail.com>
2023-03-04 16:26:03 +0200

    exercise 7 done

---
+++ csharp-basics/exercises/ClassesAndObjects/exercise-7/SavingsAccount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_7
+{
+    class SavingsAccount
+    {
+        private double rate;
+        private double balance;
+
+        public SavingsAccount(double amountOfBalance)
+        {
+            this.balance = amountOfBalance;
+        }
+
+        public void SubtractingAmountOfDeposit(double amount)
+        {
+            this.balance -= amount;
+        }
+
+        public void AddingAmountOfDeposit(double amount)
+        {
+            this.balance += amount;
+        }
+
+        public void AddingAmountOfInterest()
+        {
+            double interestRate = rate / 12;
+            double interest = balance * interestRate;
+            balance += interest;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+        public void SetInterestRate(double rate)
+        {
+            this.rate = rate;
+        }
+    }
+}
