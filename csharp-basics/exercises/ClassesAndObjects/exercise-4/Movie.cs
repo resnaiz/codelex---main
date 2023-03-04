1200acad0dfe6a51d35d43b1a4b9adf6870458ae
Arnis Pētersons <arnisp03@gmail.com>
2023-03-04 15:52:42 +0200

    exercise 4 done.

---
+++ csharp-basics/exercises/ClassesAndObjects/exercise-4/Movie.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_4
+{
+    internal class Movie
+    {
+        public string title;
+        public string studio;
+        public string rating;
+
+        public Movie(string movieTitle, string movieStudio, string movieRating)
+        {
+            this.title = movieTitle;
+            this.studio = movieStudio;
+            this.rating = movieRating;
+        }
+
+        public Movie(string movieTitle, string movieStudio)
+        {
+            this.title = movieTitle;
+            this.studio = movieStudio;
+        }
+
+        public static Movie[] GetPG(Movie[] movies)
+        {
+            Movie[] moviesWithPG = new Movie[movies.Length];
+            int counter = 0;
+
+            foreach(Movie oneMovie in movies)
+            {
+                if(oneMovie.rating == "PG")
+                {
+                    moviesWithPG[counter] = oneMovie;
+                    counter++;
+                }
+            }
+
+            Array.Resize(ref moviesWithPG, counter);
+            return moviesWithPG;
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/exercise-4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace exercise_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie firstMovie = new Movie("Casino Royale", "Eon Productions", "PG-13");
+            Movie secondMovie = new Movie("Glass", "Buena Vista International", "PG-13");
+            Movie thirdMovie = new Movie("Spider-Man: Into the Spider-Verse", "Columbia Pictures", "PG");
+
+            Movie[] allMovies = new Movie[] { firstMovie, secondMovie, thirdMovie };
+            Movie[] moviesWithPG = Movie.GetPG(allMovies);
+
+            Console.WriteLine("PG movies: ");
+            foreach(Movie movie in moviesWithPG)
+            {
+                Console.WriteLine("{0}, {1}, {2}", movie.title, movie.studio, movie.rating);
+            }
+            Console.ReadKey();
+        }
+    }
+}
