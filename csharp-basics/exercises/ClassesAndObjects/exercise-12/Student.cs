2117c0cad05615388f0afa8b5509affd9bbcaa99
Arnis Pētersons <arnisp03@gmail.com>
2023-03-05 18:39:36 +0200

    Exercise 12 done

---
+++ csharp-basics/exercises/ClassesAndObjects/exercise-12/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_12
+{
+    interface IStudent
+    {
+        string[] TestsTaken { get; set; }
+        void TakeTest(ITestpaper paper, string[] answers);
+    }
+
+    class Student :IStudent
+    {
+        public string[] TestsTaken { get; set; }
+
+        public Student()
+        {
+            this.TestsTaken = new string[] { "No tests taken" };
+        }
+
+        public void TakeTest(ITestpaper paper, string[] answers)
+        {
+            int correctRes = 0;
+
+            for (int i = 0; i < paper.MarkScheme.Length; i++)
+            {
+                if (paper.MarkScheme[i] == answers[i])
+                {
+                    correctRes++;
+                }
+            }
+
+            double correctResPercent = (double)correctRes / paper.MarkScheme.Length * 100;
+
+            string result = (decimal)correctResPercent >= Decimal.Parse(paper.PassMark.Trim('%')) ? "Passed!" : "Failed!";
+            string resultFormat = $"{paper.Subject}: {result} {string.Format("{0:F0}", correctResPercent)}%";
+
+            if (TestsTaken[0] == "No tests taken")
+            {
+                TestsTaken[0] = resultFormat;
+            }
+            else
+            {
+                string[] newTemp = TestsTaken;
+                Array.Resize(ref newTemp, TestsTaken.Length + 1);
+                newTemp[TestsTaken.Length] = resultFormat;
+                TestsTaken = newTemp;
+            }
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/exercise-12/Testpaper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_12
+{
+    interface ITestpaper
+    {
+        string Subject { get; set; }
+        string[] MarkScheme { get; set; }
+        string PassMark { get; set; }
+    }
+
+    class Testpaper : ITestpaper
+    {
+        public string Subject { set; get; }
+        public string[] MarkScheme { get; set; }
+        public string PassMark { set; get; }
+        
+        public Testpaper(string subject, string[] markScheme, string passMark)
+        {
+            this.Subject = subject;
+            this.MarkScheme = markScheme;
+            this.PassMark = passMark;
+        }
+    }
+
+
+}
