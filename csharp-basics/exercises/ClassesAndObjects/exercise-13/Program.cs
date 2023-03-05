5baea9bc928ed18800fb60d8ce5d44d7ff711a93
Arnis Pētersons <arnisp03@gmail.com>
2023-03-05 17:19:45 +0200

    exercise 13 done.

---
+++ csharp-basics/exercises/ClassesAndObjects/exercise-13/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exercise_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Smoothie firstSmoothie = new Smoothie(new string[] { "Banana" });
+            Console.WriteLine(string.Join(", ", firstSmoothie.Ingredients));
+            Console.WriteLine("£" + firstSmoothie.GetCost());
+            Console.WriteLine(firstSmoothie.GetPrice());
+            Console.WriteLine(firstSmoothie.GetName());
+
+            Console.WriteLine();
+            Smoothie secondSmoothie = new Smoothie(new string[] { "Raspberries", "Strawberries", "Blueberries" });
+            Console.WriteLine(string.Join(", ", secondSmoothie.Ingredients));
+            Console.WriteLine("£" + secondSmoothie.GetCost());
+            Console.WriteLine(secondSmoothie.GetPrice());
+            Console.WriteLine(secondSmoothie.GetName());
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/exercise-13/Smoothie.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace exercise_13
+{
+    internal class Smoothie
+    {
+        public string[] Ingredients { get; set; }
+
+        public Smoothie(string[] ingredients)
+        {
+            this.Ingredients = ingredients;
+        }
+
+        public double GetCost()
+        {
+            double totalCost = 0.0;
+
+            foreach (string ingredient in Ingredients)
+            {
+                switch (ingredient)
+                {
+                    case "Strawberries":
+                        totalCost += 1.5;
+                        break;
+                    case "Banana":
+                        totalCost += 0.5;
+                        break;
+                    case "Mango":
+                        totalCost += 2.5;
+                        break;
+                    case "Blueberries":
+                        totalCost += 1.0;
+                        break;
+                    case "Raspberries":
+                        totalCost += 1.0;
+                        break;
+                    case "Apple":
+                        totalCost += 1.75;
+                        break;
+                    case "Pineapple":
+                        totalCost += 3.5;
+                        break;
+                }
+            }
+
+            return totalCost;
+        }
+
+        public string GetPrice() 
+        {
+            double anotherCost = GetCost();
+            double price = anotherCost + (anotherCost * 1.5);
+            return string.Format("£{0:0.00}", price);
+        }
+
+        public string GetName()
+        {
+            string name;
+
+            if (Ingredients.Length == 0)
+            {
+                name = "No ingredients in the smoothie.";
+            }
+            else if (Ingredients.Length == 1)
+            {
+                name = $"{Ingredients[0]} Smoothie";
+            }
+            else
+            {
+                Array.Sort(Ingredients);
+
+                for (int i = 0; i < Ingredients.Length; i++)
+                {
+                    if (Ingredients[i].EndsWith("berries"))
+                    {
+                        Ingredients[i] = Ingredients[i].Replace("berries", "berry");
+                    }
+                }
+
+                name = $"{string.Join(" ", Ingredients)} Fusion";
+            }
+
+            return name;
+        }
+    }
+}
