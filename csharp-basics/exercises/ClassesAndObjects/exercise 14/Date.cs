4ab6d857dabbe0f2b5c7835608d227c4b2577aaf
Arnis Pētersons <arnisp03@gmail.com>
2023-03-05 17:39:30 +0200

    exercise 14 done.

---
+++ csharp-basics/exercises/ClassesAndObjects/exercise 14/Date.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_14
+{
+    internal class Date
+    {
+        int year;
+        int month;
+        int day;
+
+        public Date(int yearInfo, int monthInfo, int dayInfo)
+        {
+            this.year = yearInfo;
+            this.month = monthInfo;
+            this.day = dayInfo;
+        }
+
+        public string WeekdayInDutch()
+        {
+            DateTime dateTime = new DateTime(year, month, day);
+            return dateTime.ToString("dddd", new System.Globalization.CultureInfo("nl-NL"));
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/exercise 14/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace exercise_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Date firstDate = new Date(1970, 9, 21);
+            Date secondDate = new Date(1945, 9, 2);
+            Date thirdDate = new Date(2001, 9, 11);
+
+            Console.WriteLine(firstDate.WeekdayInDutch());
+            Console.WriteLine(secondDate.WeekdayInDutch());
+            Console.WriteLine(thirdDate.WeekdayInDutch());
+        }
+    }
+}
