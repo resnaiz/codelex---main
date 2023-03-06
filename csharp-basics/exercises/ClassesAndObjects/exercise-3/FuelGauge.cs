8c9e2a02c216fdd8441eef075598022f4c14ce0e
Arnis Pētersons <arnisp03@gmail.com>
2023-03-06 11:43:52 +0200

    exercise 3 done

---
+++ csharp-basics/exercises/ClassesAndObjects/exercise-3/FuelGauge.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_3
+{
+    public class FuelGauge
+    {
+        public int currentFuelAmount;
+
+        public FuelGauge() 
+        {
+            this.currentFuelAmount = 0;
+        }
+
+        public int GetFuelAmount()
+        {
+            return currentFuelAmount;
+        }
+
+        public void RefuelLimit()
+        {
+            if(currentFuelAmount < 70)
+            {
+                currentFuelAmount++;
+            }
+        }
+
+        public void FuelLow()
+        {
+            if(currentFuelAmount > 0)
+            {
+                currentFuelAmount--;
+            }
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/exercise-3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace exercise_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Odometer newOdometer = new Odometer();
+
+            for(int i = 0; i < 70; i++)
+            {
+                newOdometer.fuelGauge.RefuelLimit();
+            }
+
+            while(newOdometer.fuelGauge.GetFuelAmount() > 0)
+            {
+                newOdometer.IncreaseMileage();
+                Console.WriteLine("Current mileage is - " + newOdometer.GetMileage() + " km.");
+                Console.WriteLine("Current fuel level: " + newOdometer.fuelGauge.GetFuelAmount() + " L");
+            }
+
+            Console.WriteLine("Out of fuel.");
+        }
+    }
+}
