209b2cd416260bafcc4416a5af9b8ae918708bad
Arnis Pētersons <arnisp03@gmail.com>
2023-03-07 17:59:50 +0200

    FlightPlanner done.

--- csharp-basics/exercises/Collections/FlightPlanner/Program.cs
+++ csharp-basics/exercises/Collections/FlightPlanner/Program.cs
@@ -2,21 +2,84 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FlightPlanner
 {
     class Program
     {
-        private const string Path = "../FlightPlanner/flights.txt";
+        private const string Path = "../../flights.txt";
 
         private static void Main(string[] args)
         {
             var readText = File.ReadAllLines(Path);
-            foreach (var s in readText)
+
+            List<string> flightInfo = new List<string>();
+
+            Console.WriteLine("What would you like to do: ");
+            Console.WriteLine("To display list of the cities press 1: ");
+            Console.WriteLine("To exit program press - #");
+            var userInput = Console.ReadLine();
+
+            bool returnStartingPoint = false;
+
+            if(userInput == "1")
             {
-                Console.WriteLine(s);
+                Console.WriteLine("To select the starting city press 1");
+                var otherUserInput = Console.ReadLine();
+
+                if (otherUserInput == "1")
+                {
+                    foreach (var flight in readText)
+                    {
+                        string[] cityInfo = flight.Split('-');
+
+                        string selectedCity = cityInfo[0].Trim();
+
+                        flightInfo.Add(selectedCity);
+                    }
+
+                    Console.WriteLine(string.Join(", ", flightInfo.Distinct()));
+
+                    var startingPoint = "";
+                    List<string> dest = new List<string>();
+
+                    while (!returnStartingPoint)
+                    {
+                        Console.WriteLine("From where?");
+                        var cityName = Console.ReadLine();
+
+                        if (string.IsNullOrEmpty(startingPoint))
+                        {
+                            startingPoint = cityName;
+                        }
+
+                        dest.Add(cityName);
+
+                        if (startingPoint == dest.Last() && dest.Count() > 1)
+                        {
+                            returnStartingPoint = true;
+                            break;
+                        }
+
+                        Console.WriteLine($"Destinations from {cityName}- ");
+
+                        foreach (var flight in readText)
+                        {
+                            string[] flightRoute = flight.Split(new char[] { '-', '>' });
+                            string selectedC = flightRoute[0].Trim();
+                            string to = flightRoute[flightRoute.Length - 1].Trim();
+
+                            if (selectedC == cityName)
+                            {
+                                Console.WriteLine(to);
+                            }
+                        }
+                    }
+                }
+            }
+            else if(userInput == "#")
+            {
+                return;
             }
         }
     }
