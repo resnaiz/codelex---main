8be649476da0c18e8c33c4e26eb66ea2f6260cbb
Arnis Pētersons <arnisp03@gmail.com>
2023-03-08 18:00:45 +0200

    BankAccount changed

--- csharp-basics/exercises/ClassesAndObjects/BankAccount/BankAcc.cs
+++ csharp-basics/exercises/ClassesAndObjects/BankAccount/BankAcc.cs
@@ -8,28 +8,28 @@ namespace BankAccount
 {
     internal class BankAccount
     {
-        private string name;
-        private double balance;
+        private string _name;
+        private double _balance;
 
         public BankAccount(string name, double balance)
         {
-            this.name = name;
-            this.balance = balance;
+            _name = name;
+            _balance = balance;
         }
 
         public string ShowUserNameAndBalance()
         {
             string newBalance;
 
-            if (balance < 0)
+            if (_balance < 0)
             {
-                newBalance = string.Format("-${0:F2}", -balance);
+                newBalance = string.Format("-${0:F2}", -_balance);
             }
             else
             {
-                newBalance = string.Format("${0:F2}", balance);
+                newBalance = string.Format("${0:F2}", _balance);
             }
-            return name + ", " + newBalance;
+            return _name + ", " + newBalance;
         }
     }
 }
